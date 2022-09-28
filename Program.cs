795fee503195140c94eaa638149dfd38bf63bf2a
Kulakov Anton <kulakov_3000@mail.ru>
2022-09-28 14:25:22 +0300

    Домашняя работа

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+string[] array = new string[] { "hello", "2", "world", ":-)" };
+
+// PrintArray(array, array.Length);
+// Console.WriteLine();
+
+string[] arraySort = new string[array.Length];
+
+int j = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        arraySort[j] = array[i];
+        j++;
+    }
+}
+
+PrintArray(arraySort, j);
+
+
+void PrintArray(string[] array, int j)
+{
+    Console.Write("[");
+
+    for (int i = 0; i < j - 1; i++)
+    {
+        Console.Write($"{array[i]},");
+    }
+
+    Console.Write(array[j - 1]);
+    Console.Write("]");
+}
